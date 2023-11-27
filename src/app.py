28929518447fc2f034c4c252ec36f5acd4bbbc23
mjd <mjdarvishi1374@gmail.com>
2023-11-27 10:08:46 +0100

    add flask for showing ui

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template,request
+
+# print the updated sys.path
+
+from wiki_data_source import get_medical_title,get_non_medical_title,get_wikipedia_text
+from bag_of_word_model import check
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    data=get_medical_title(number=50)
+    data1=get_non_medical_title(number=50)
+    return render_template('home.html',data=[data,data1])
+
+@app.route('/', methods=['POST'])
+def index():
+    user_input = request.form.get('data')
+    result=check(user_input)
+    if result==None:
+        return render_template('result.html', user_input='Your title has no wiki page')
+    return render_template('result.html', user_input=result)
+
+@app.route('/detail/<name>')
+def detail_page(name):
+    data=get_wikipedia_text(name)
+    # Render the detail page with the parameters
+    return render_template('detail_page.html',data=data,title=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/bag_of_word_model.py
+++ src/bag_of_word_model.py
@@ -9,63 +9,69 @@ from sklearn.preprocessing import LabelEncoder
 from wiki_data_source import get_wikipedia_text,get_medical_title,get_non_medical_title
 from data_repository import get_medical_content,get_non_medical_content
 
-# Fetch Wikipedia content for medical topics
+      # Fetch Wikipedia content for medical topics
 medical_content = get_medical_content()
 
-# Fetch Wikipedia content for non-medical topics
+    # Fetch Wikipedia content for non-medical topics
 non_medical_content = get_non_medical_content()
-
-# Combine the content for processing
-corpus = medical_content + non_medical_content
-
 labels = ["Medical"] * len(medical_content) + ["Non-Medical"] * len(non_medical_content)
 
-# Create bag-of-words representation
+
 vectorizer = CountVectorizer()
-X_bow = vectorizer.fit_transform(corpus)
 
-# Encode labels
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(labels)
+def create_model():
+    corpus = medical_content + non_medical_content
 
-# Check the unique classes in y
-unique_classes = set(y)
-if len(unique_classes) < 2:
-    raise ValueError("At least two classes are needed in the data.")
+    X_bow = vectorizer.fit_transform(corpus)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_bow, y, test_size=0.2, random_state=42)
+    # Encode labels
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(labels)
 
-# Train a logistic regression model
-model = LogisticRegression()
-model.fit(X_train, y_train)
+    # Check the unique classes in y
+    unique_classes = set(y)
+    if len(unique_classes) < 2:
+        raise ValueError("At least two classes are needed in the data.")
 
-# Make predictions on the test set
-y_pred = model.predict(X_test)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X_bow, y, test_size=0.2, random_state=42)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred, target_names=["Medical", "Non-Medical"])
+    # Train a logistic regression model
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
 
-# Print results
-print(f"Accuracy: {accuracy:.2f}")
-print("\nClassification Report:")
-print(classification_rep)
+    # Make predictions on the test set
+    y_pred = model.predict(X_test)
+
+    # Evaluate the model
+    accuracy = accuracy_score(y_test, y_pred)
+    classification_rep = classification_report(y_test, y_pred, target_names=["Medical", "Non-Medical"])
+
+    # Print results
+    print(f"Accuracy: {accuracy:.2f}")
+    print("\nClassification Report:")
+    print(classification_rep)
+    return model
+
+main_model=create_model()
 
 
+def check(input):
+    model=main_model
+      # Fetch Wikipedia content for the new topic
+    new_content = get_wikipedia_text(input)
+    if new_content ==None:
+        return None
 
-# Choose a new topic for testing
-new_topic = get_non_medical_title(number=1)
+    # Transform the new content using the same vectorizer
+    X_new = vectorizer.transform([new_content])
 
-# Fetch Wikipedia content for the new topic
-new_content = get_wikipedia_text(new_topic[0])
+    # Make predictions using the trained model
+    predicted_class = model.predict(X_new)[0]
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(labels)
+    predicted_category = label_encoder.inverse_transform([predicted_class])[0]
 
-# Transform the new content using the same vectorizer
-X_new = vectorizer.transform([new_content])
-
-# Make predictions using the trained model
-predicted_class = model.predict(X_new)[0]
-predicted_category = label_encoder.inverse_transform([predicted_class])[0]
-
-# Print the results
-print(f"Predicted Category for '{new_topic}': {predicted_category}")
+    # Print the results
+    print(f"Predicted Category for '{input}': {predicted_category}")
+    return predicted_category
